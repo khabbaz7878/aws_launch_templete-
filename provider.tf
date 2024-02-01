efe686f1c9cf7e6f814583033a8bcb877b8ca7f0
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-01-31 20:38:05 -0500

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region = "us-east-1"
+
+  # Optional - access key and secret key
+  access_key = "YOUR_ACCESS_KEY" 
+  secret_key = "YOUR_SECRET_KEY"
+
+  # Optional - profile name from credential file
+  profile = "default"
+}
